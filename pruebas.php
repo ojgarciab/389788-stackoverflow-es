a8ef3624daa559f11b1ca63d420afbb7ec7a9418
Oscar Garcia <oscar.github@linaresdigital.com>
2020-09-24 08:26:27 +0200

    Changed XML inside ZIP

--- pruebas.php
+++ pruebas.php
@@ -1,13 +1,16 @@
 <?php
-$file = 'archivo.zip';
+$file = 'data/archivo.zip';
 $code = 'archivo';
 if (file_exists($file)) {
-    $xml = @simplexml_load_file('zip://'. $file .'#'. $code .'.xml');
+    $xml = simplexml_load_file('zip://'. $file .'#'. $code .'.xml');
     if ($xml === false) {
         die('Error abriendo el archivo XML dentro del ZIP');
     }
-    $name = $xml->{'cac:Party'}->{'cac:PartyIdentification'}->{'cbc:ID'};
-    echo $name, PHP_EOL;
+    $name = $xml->xpath('//cac:AccountingCustomerParty/cac:Party/cac:PartyIdentification/cbc:ID');
+    //->{'cac:AccountingCustomerParty'}->{'cac:Party'}->{'cac:PartyIdentification'}->{'cbc:ID'};
+    var_export($name);
+    //$name = $xml->{'cac:Party'}->{'cac:PartyIdentification'}->{'cbc:ID'};
+    echo $name[0], PHP_EOL;
 } else {
     die('Archivo no encontrado');
 }
